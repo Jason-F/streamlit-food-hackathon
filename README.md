e3113570b7f8cd990448a3e6458cc867b42871a6
Jason Farrell <jasonfarrell.v1@gmail.com>
2024-11-14 06:56:33 +0200

    main foods streamlit setup

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
-# streamlit-food-hackathon
-streamlit app for the woolies food hackathon
-
 
 ## Development
 * Create and activate a venv: `python3 -m venv venv` and `source venv/Scripts/activate`
 * Install requirements: `pip3 install -r requirements.txt`
 * Run streamlit: `streamlit run app.py`
 
-## Pip installable alternative
-If you wish to `pip install` the functionality provided by this plugin checkout [streamlit-image-comparison](https://github.com/fcakyon/streamlit-image-comparison)
-
-## References
-- https://juxtapose.knightlab.com/
-- https://github.com/innerdoc/streamlit-timeline
-- https://discuss.streamlit.io/t/restrict-download-of-images-videos-on-streamlit-app/11718/2
+This was made for the Woolies Hackathon 2024-11-14
--- app.py
+++ app.py
@@ -5,34 +5,89 @@ from PIL import Image
 import requests
 
 import pathlib
+import pandas as pd
+import cv2
+import numpy as np
+from datetime import datetime
+from matplotlib import pyplot as plt
+from IPython.display import Image
+from skimage.metrics import structural_similarity
+import os
+from streamlit_image_comparison import image_comparison
+from PIL import Image
+#%matplotlib inline
 
-STREAMLIT_STATIC_PATH = (
-    pathlib.Path(st.__path__[0]) / "static"
-)  # at venv/lib/python3.9/site-packages/streamlit/static
+def check_image(image1, image2):
+    # To check if images exist
+    from os.path import exists
+    if exists(image1) and exists(image2):
+        print("Files are available. Images should have the same shape")
+        img1 = cv2.imread(image1)
+        img2 = cv2.imread(image2)
+        st.write("Image file 1:", image1, " shape =", img1.shape)
+        st.write("Image file 2:", image2, " shape =", img2.shape)
+        if img1.shape == img2.shape:
+            st.write('`Matched encoding`')
+        else:
+            st.write('`Unmatched encoding`')
+        
+    else:
+        print("Error!")
+        if not exists(image1):
+            print(image1, ' is not available')
+        if not exists(image2):
+            print(image2, ' is not available')
+def main():
 
-IMG1 = "img1.png"
-IMG2 = "img2.png"
+    page_options = ["Select Data","Structural Similarity Index", "Computer Vision Model", "GPT", "About Us"]
 
-DEFAULT_IMG1_URL = (
-    "https://juxtapose.knightlab.com/static/img/Sochi_11April2005.jpg"
-)
-DEFAULT_IMG2_URL = (
-    "https://juxtapose.knightlab.com/static/img/Sochi_22Nov2013.jpg"
-)
+    # -------------------------------------------------------------------
+    # ----------------- !! Main code section !! -------------------------
+    # -------------------------------------------------------------------
+    page_selection = st.sidebar.selectbox("Choose Option", page_options)
+    if page_selection == "Select Data":
+        # Header contents
+        st.write('# Planogram Compliance Model')
+        st.image('images/logo1.jpg')
+        st.image('images/header1.png')
+        st.write('## Upload planogram and realogram data')
+        
+        planogram_file = st.file_uploader("Choose Planogram",type = 'jpeg')
+        realogram_file = st.file_uploader("Upload Realogram",type = 'jpeg')
+        if st.button('Submit'):
 
-def fetch_img_from_url(url: str) -> Image:
-    img = Image.open(requests.get(url, stream=True).raw)
-    return img
+            image_comparison(
+                img1=Image.open(planogram_file),
+                img2=Image.open(realogram_file),
+                label1="Planogram",
+                label2="Realogram",
+            )
+            plan_filepath = 'images/'+planogram_file.name
+            real_filepath = 'images/'+realogram_file.name
+            check_image(plan_filepath,real_filepath)
+            
+        
+    if page_selection == "Structural Similarity Index":
+        st.title("Structural Similarity Index")
+        st.markdown("""Identifying the differences between realogram and planogram""")
 
-form = st.form(key="Image comparison")
-img1_url = form.text_input("Image one url", value=DEFAULT_IMG1_URL)
-img1 = fetch_img_from_url(img1_url)
-img1.save(STREAMLIT_STATIC_PATH / IMG1)
+    if page_selection == "Computer Vision Model":
+        st.title("Computer Vision")
+        st.markdown("""Utilizing Azure's Computer Vision model""")
 
-img2_url = form.text_input("Image two url", value=DEFAULT_IMG2_URL)
-img2 = fetch_img_from_url(img2_url)
-img2.save(STREAMLIT_STATIC_PATH / IMG2)
 
-submit = form.form_submit_button("Submit")
-if submit:
-    juxtapose(IMG1, IMG2)
+    if page_selection == "GPT":
+        st.title("GPT 4o")
+        st.markdown("""Utilizing GPT for descriptive anaylsis""")
+
+    if page_selection == "About Us":
+        st.info('**The legends who made it happen**')
+        st.write('**Jason Farrell** - Senior Data Scientist')
+        st.write('**Rod Morgan** - Senior Data Scientist')
+        st.write('**Keanu Gertse** - Data Scientist')
+        
+        st.image('images/logo1.jpg')
+
+
+if __name__ == '__main__':
+    main()
